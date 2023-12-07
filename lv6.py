ba741256c136e015cecc9b8c40753bd17ec0ff19
vctrubio <vctrubio@gmail.com>
2023-12-07 12:29:15 +0100

    debug_station

--- lv6.py
+++ lv6.py
@@ -1,4 +1,7 @@
-# create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+import sys
+
+## create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+
 def create_phone_numbers_v1(n):
     return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
 
@@ -11,5 +14,22 @@ def create_phone_number_v3(n):
     n = ''.join(map(str,n))
     return '(%s) %s-%s'%(n[:3], n[3:6], n[6:])
 
+#####################################################################################
+
+ft_call = {
+    'cv1' : create_phone_numbers_v1
+    }
+
+def debug_station(*args):
+    for i in args:
+        print(i)
+
+#=> take 2 arguments from the command line, one to state what function to call, the other to pass a string to that function
 if __name__ == "__main__":
-    ##take 2 arguments from the command line, one to state what function to call, the other to pass a string to that function
+    try:
+        print(sys.argv[0])
+        debug_station(sys.argv)
+    except Exception:
+        print("ERROR: no argv")
+
+
