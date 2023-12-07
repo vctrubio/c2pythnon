9723ef1b6286bd8ebed053f6596a1ac007d10c6e
vctrubio <vctrubio@gmail.com>
2023-12-07 17:33:53 +0100

    capitalize + spin_words

--- lv6.py
+++ lv6.py
@@ -3,6 +3,10 @@ import libft
 
 ## create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
 
+def debug_station(*args):
+    for i in args:
+        print(i)
+
 def norminette(n):
     if len(n) == 10:
         return True
@@ -27,13 +31,35 @@ ft_call_0712 = {
     'v3': create_phone_number_v3,
 }
 
-####################
+## reverse words if greater than 5 # => 
+def spin_words_v1(sentence):
+    lst = sentence.split()
+    rtn = []
+    for index, i in enumerate(lst):
+        if len(i) <= 5:
+            rtn.append(i)
+        else:
+            rtn.append(i[::-1])
+    str1 = ' '.join(rtn)
+    return str1
 
-def debug_station(*args):
-    for i in args:
-        print(i)
+def spin_words_v2(sentence):
+    words = sentence.split()
+    rev_words = [w[::-1] if len(w) > 5 else w for w in words]
+    return ' '.join(rev_words)
 
+ft_call_0512 = {
+    'v1': spin_words_v1,
+    'v2': spin_words_v2,
+}
+
+## to_capitalize
+def to_jaden_case(string):
+    return ' '.join([word.capitalize() for word in string.split()])
+
+## function_callers
 def ft_0712(create_phone_number_v, phone_numer):
+    print(ft_0712.__name__)
     try:
         ft_name = create_phone_number_v
         if ft_name in ft_call_0712 and norminette(phone_numer):
@@ -41,12 +67,23 @@ def ft_0712(create_phone_number_v, phone_numer):
             parsed_phone_number = rtn_ft_call(phone_numer)
             print(libft.style.GREEN + "√" + libft.style.RESET + parsed_phone_number)
         else:
-            raise Exception ("No ft_call- found")
+            raise Exception ("[0712]: No ft_call found")
     except Exception as e:
         print(e)
 
+def ft_0512(version, str):
+    print(ft_0512.__name__)
+    try:
+        if version in ft_call_0512:
+            rtn_ft_call = ft_call_0512[version]
+            parsed_str = rtn_ft_call(str)
+            print(libft.style.GREEN + "√" + libft.style.RESET + parsed_str)
+    except:
+        print(libft.style.RED + "[0512] could not call function" + libft.style.RESET)
+
 ft_date = {
     '0712': ft_0712,
+    '0512': ft_0512,
 }
 
 #=> take 2 arguments from the command line, one to state what function to call, the other to pass a string to that function
