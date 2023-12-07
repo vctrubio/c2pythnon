c063fcc3bc6b39e270d8d415372de3d0ae128ee3
vctrubio <vctrubio@gmail.com>
2023-12-07 12:06:27 +0100

    dec.7

---
+++ lv6.py
@@ -0,0 +1,15 @@
+# create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+def create_phone_numbers_v1(n):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+def create_phone_number_v2(n):
+  str1 =  ''.join(str(x) for x in n[0:3])
+  str2 =  ''.join(str(x) for x in n[3:6])
+  str3 =  ''.join(str(x) for x in n[6:10])
+
+def create_phone_number_v3(n):
+    n = ''.join(map(str,n))
+    return '(%s) %s-%s'%(n[:3], n[3:6], n[6:])
+
+if __name__ == "__main__":
+    ##take 2 arguments from the command line, one to state what function to call, the other to pass a string to that function
