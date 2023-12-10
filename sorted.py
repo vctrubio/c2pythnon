6b3b53ea31baa0dd74649fe64036ea2ef15021f6
vctrubio <vctrubio@gmail.com>
2023-12-10 10:30:32 +0100

    formatted

--- sorted.py
+++ sorted.py
@@ -1,4 +1,4 @@
-from operator import itemgetter, attrgetter
+from operator import itemgetter
 
 roman_numerals = {1000: 'M',
                   900: 'CM',
@@ -15,6 +15,24 @@ roman_numerals = {1000: 'M',
                   1: 'I'
                   }
 
+
+class Student:
+    def __init__(self, name, grade, age):
+        self.name = name
+        self.grade = grade
+        self.age = age
+
+    def __repr__(self):
+        return repr((self.name, self.grade, self.age))
+
+
+student_objects = [
+    Student('john', 'A', 15),
+    Student('jane', 'B', 12),
+    Student('dave', 'B', 10),
+]
+
+
 def test_sort():
     global roman_numerals
     arr = []
@@ -22,24 +40,23 @@ def test_sort():
     for key in roman_numerals:
         a2 += roman_numerals[key] + ' '
         arr.append(key)
-    
+
     print(arr)
     print(a2)
     print("-- --")
 
-    arr.sort() # sorted(arr)
-    a3 = a2.split()[::-1] # sorted(a2.split(), reverse=True)
+    arr.sort()  # sorted(arr)
+    a3 = a2.split()[::-1]  # sorted(a2.split(), reverse=True)
     print(arr)
     print(a3)
     print("-- --")
 
-    a4 = sorted(a3, key=lambda ptr: ptr[0]) # , reverse=True (can append)
-    a4 = sorted(a3, key=itemgetter(1)) # , reverse=True (can append)
-    a5 = sorted(a3, key=lambda word: next(key for key, value in roman_numerals.items() if value == word))
+    a4 = sorted(a3, key=lambda ptr: ptr[0])  # , reverse=True (can append)
+    a4 = sorted(a3, key=itemgetter(0))  # ^ same as above
+    a5 = sorted(a3, key=lambda word: next(
+        key for key, value in roman_numerals.items() if value == word))
     print(a4)
     print(a5)
-
-
 
 
 if __name__ == '__main__':
