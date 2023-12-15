9d2e5abee4794807d66a3fcd2b8e3b554a22ee95
vctrubio <vctrubio@gmail.com>
2023-12-15 23:11:22 +0100

    tip by tip, we know can search model and json. now gn

--- dates_and_names_versions/v3.py
+++ dates_and_names_versions/v3.py
@@ -11,14 +11,15 @@ files_to_scrap = ['day.json', 'location.json']
 
 # NOTE _underscores are meant to be imported | _underscore_ is meant to be *inheritate*
 
+
 @dataclass
 class Location:
     id: int = 0
-    _latitude: float = 0 #field(default=0, metadata={'unit': 'degrees'})
+    _latitude: float = 0  # field(default=0, metadata={'unit': 'degrees'})
     _longitude: float = 0
     _address: str = field(default=None)
 
-    
+
 @dataclass
 class Day:
     id: int
@@ -37,31 +38,42 @@ class Entity():
 
     def __post_init__(self):
         self._name = self._location._address
-    
+
     def __str__(self):
         return f"Entity: {self._name} {self._days}"
 
 ####################
+
+
 def get_json() -> dict:
     rtn = {}
     for file_name in files_to_scrap:
         try:
             with open(f'{API_FETCH_DIR}{file_name}', 'r') as file:
-                    data = json.load(file)
-                    data['_model'] = os.path.splitext(file_name)[0].title()
-                    rtn[file_name.upper()] = data
+                data = json.load(file)
+                data['_model'] = os.path.splitext(file_name)[0].title()
+                rtn[file_name.upper()] = data
         except FileNotFoundError:
-            print(f".!. File {file_name} not found in {os.getcwd()}{API_FETCH_DIR[1::]}")
+            print(
+                f".!. File {file_name} not found in {os.getcwd()}{API_FETCH_DIR[1::]}")
     return rtn
 
 
 def if_key_in_class(key, Class):
-    search_filter = '_' + key # => in order to match our attributes
+    search_filter = '_' + key  # => in order to match our attributes
     if search_filter in Class.__dict__:
         print(f'debug: yes {key}')
     else:
         print(f'debug: no {key}')
     return search_filter in Class.__dict__
+
+
+def if_key_in_class_model_name(key, model_name):
+    # search_filter = key[1:]
+    search_filter = key
+    print(f'debug: model_name {search_filter} :vs: {model_name}')
+    return search_filter in model_name
+
 
 def json_to_class_population(data, Class):
     def iterate_dict(d):
@@ -73,14 +85,47 @@ def json_to_class_population(data, Class):
 
     iterate_dict(data)
 
+# parse json into models
+
+
+def parse_json_into_models(data_from_json):
+    class_constructor = []
+    to_import = [Location]
+    # to_import = [Location, Day]
+    class_names = [cls.__name__ for cls in to_import]
+
+    for _, values in data_from_json.items():
+        if '_model' in values:
+            if (values['_model']) in class_names:
+                class_constructor.append(values)
+                # print('yes')
+                # print(f'OK: {values["_model"]} in ')
+
+    for it in class_constructor:
+        try:
+            cls = globals()[it['_model']]
+            class_attrs = cls.__match_args__
+            filter_dict_keys = {}
+            for attr in class_attrs:
+                if attr.startswith('_'):
+                    attr_trim = attr[1:]
+                    if attr_trim in it:
+                        filter_dict_keys[attr_trim] = it[attr_trim]
+            print(filter_dict_keys)
+        except KeyError:
+            print(
+                f'KeyError: {it["_model"]} not found in globals() | SOMETHING WENT WRONG')
+
+        # clean unecesrray keys
+        # call constructor as dict
+
+
 if __name__ == '__main__':
     data_from_json = get_json()
-    classes = [Location, Day] # (Entity is made by us)
-    
-    #make zip/pai between json model and class model
-    #parse json into models
-    print(data_from_json)
+    constructor = parse_json_into_models(data_from_json)
 
-    json_to_class_population(data_from_json, Location)
+#    print(data_from_json)
+#    json_to_class_population(data_from_json, Location)
 
 
+#    print(Location.__name__)
