c5da2be5b3c74c184920ded3d90ebe318364f8ac
vctrubio <vctrubio@gmail.com>
2023-12-24 21:46:29 +0100

    init01

--- small_scale_projects/ticket_handler/models.py
+++ small_scale_projects/ticket_handler/models.py
@@ -1,11 +1,27 @@
+from typing import List, Optional
+from sqlmodel import SQLModel, Field, create_engine, Session
 
 
-class User:
-    username: str
-    #has many tickets
+class User(SQLModel):
+    username: str = Field(unique=True)
+    # has many tickets
 
-class Ticket:
+
+class Ticket(SQLModel):
     price: int
     name: str
-    paid: bool
-    user: User #Has on user
+    paid: bool = Field(default=False)
+    user: User # has one user
+
+
+class GroupTicket(SQLModel):
+    tickets: List[Ticket] = []
+    total_price: Optional[int] = None
+
+
+if __name__ == '__main__':
+    migel = User(username='migel')
+    myticket = Ticket(name="11", price=11, user=migel)
+    mygroup = GroupTicket()
+    print(myticket)
+    print(mygroup)
