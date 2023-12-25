260ea02052290ebe863f79295690008bd02ac122
vctrubio <vctrubio@gmail.com>
2023-12-25 14:40:54 +0100

    connected to pony

--- small_scale_projects/ticket_handler/models.py
+++ small_scale_projects/ticket_handler/models.py
@@ -21,8 +21,9 @@ if __name__ == '__main__':
         db.generate_mapping(create_tables=True)
 
         # Try to perform a database operation
-        with db_session:
-            User.select()[:1]
+        @db_session:
+        User.select()[:1]
+
         print("Database connection successful.")
     except Exception as e:
         print(f"Database connection failed: {e}")
